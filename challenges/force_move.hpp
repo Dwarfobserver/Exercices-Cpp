60300857365df67dbc4c63b1e12c1ac562537864
Sidney Congard <sidney.congard@gmail.com>
2018-06-14 18:20:22 -0400

    made force_move more general

--- challenges/force_move.hpp
+++ challenges/force_move.hpp
@@ -8,19 +8,19 @@
 // On souhaite obtenir une erreur de compilation si le type est const :
 // En effet, on veut s'assurer qu'il pourra être move (et donc modifié).
 
-// On peut affichier une erreur à la compilation :
+// On peut afficher une erreur à la compilation :
 
 template <class T>
-T&& force_move(T& t) {
+std::remove_reference_t<T>&& force_move(T&& t) {
     static_assert(!std::is_const_v<T>, "The reference musn't be const");
-    return static_cast<T&&>(t);
+    return static_cast<std::remove_reference_t<T>&&>(t);
 }
 
 // Sinon, on peut supprimer la fonction dans le cas où le type est const :
 
 template <class T>
-T&& force_move2(T& t) {
-    return static_cast<T&&>(t);
+std::remove_reference_t<T>&& force_move2(T&& t) {
+    return static_cast<std::remove_reference_t<T>&&>(t);
 }
 
 template <class T>
